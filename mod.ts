2fdc68d84b67114061a5949e9fdb061d9b54512d
SnO2WMaN <me@sno2wman.net>
2022-11-17 02:10:52 +0900

    prepare auth

--- mod.ts
+++ mod.ts
@@ -9,11 +9,16 @@ const router = new Router();
 router.post("/graphql", async ({ request, response }) => {
   const { query, variables, operationName } = await request.body().value;
 
+  const accessToken = request.headers.get("Authorization")?.split("Bearer ")?.[1];
+
   response.body = await graphql({
     schema: schema,
     source: query,
     variableValues: variables,
     operationName: operationName,
+    contextValue: {
+      accessToken,
+    },
   });
   return;
 });
--- schema.ts
+++ schema.ts
@@ -1,4 +1,12 @@
-import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import {
+  GraphQLFieldConfig,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
 
 export const UserType = new GraphQLObjectType<{ id: string }>({
   name: "User",
@@ -7,21 +15,67 @@ export const UserType = new GraphQLObjectType<{ id: string }>({
   },
 });
 
-export const QueryType = new GraphQLObjectType({
-  name: "Query",
+export const TagType = new GraphQLObjectType({
+  name: "Tag",
   fields: {
-    user: {
-      type: new GraphQLNonNull(UserType),
-      args: { id: { type: GraphQLString } },
-      resolve(_, args) {
-        console.dir(args);
-        return { name: "?" };
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve(source, _args, context) {
+        return source.name;
       },
     },
+  },
+});
+
+export const ArticleType = new GraphQLObjectType<{
+  id: string;
+  tags: string[];
+}>({
+  name: "Article",
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLID) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    tags: {
+      type: new GraphQLNonNull(new GraphQLList(TagType)),
+      resolve({ tags }) {
+        return tags.map((tagId) => ({ id: tagId, name: tagId }));
+      },
+    },
+  },
+});
+
+export const GetUser: GraphQLFieldConfig<
+  unknown,
+  { accessToken: string },
+  { id: string }
+> = {
+  type: new GraphQLNonNull(UserType),
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+  },
+  resolve(_, args, ctx) {
+    console.dir(ctx);
+    return { name: "?" };
+  },
+};
+
+export const QueryType = new GraphQLObjectType({
+  name: "Query",
+
+  fields: {
+    user: GetUser,
     users: {
       type: new GraphQLNonNull(new GraphQLList(UserType)),
       resolve(_, args) {
         return [];
+      },
+    },
+    articles: {
+      type: new GraphQLNonNull(new GraphQLList(ArticleType)),
+      resolve(_source, _args, context) {
+        return [
+          { id: "1", title: "a", tags: ["1", "2"] },
+        ];
       },
     },
   },
